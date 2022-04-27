e642a0ffa31502644a881a188b458941d8341eab
Emrehan Baysal <81330668+bymrhan@users.noreply.github.com>
2022-04-27 07:32:41 +0300

    Ean13barcode
    Ean13 e göre barcode oluşturma uygulaması

---
+++ barkodolustur/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace barkodolustur
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btncountrycode = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnManufacturerCode = new System.Windows.Forms.Label();
+            this.btnProductCode = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(306, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 140);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btncountrycode
+            // 
+            this.btncountrycode.Location = new System.Drawing.Point(132, 25);
+            this.btncountrycode.Name = "btncountrycode";
+            this.btncountrycode.Size = new System.Drawing.Size(100, 20);
+            this.btncountrycode.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "create barcode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Country code";
+            // 
+            // btnManufacturerCode
+            // 
+            this.btnManufacturerCode.AutoSize = true;
+            this.btnManufacturerCode.Location = new System.Drawing.Point(11, 65);
+            this.btnManufacturerCode.Name = "btnManufacturerCode";
+            this.btnManufacturerCode.Size = new System.Drawing.Size(98, 13);
+            this.btnManufacturerCode.TabIndex = 3;
+            this.btnManufacturerCode.Text = "Manufacturer Code";
+            // 
+            // btnProductCode
+            // 
+            this.btnProductCode.AutoSize = true;
+            this.btnProductCode.Location = new System.Drawing.Point(40, 96);
+            this.btnProductCode.Name = "btnProductCode";
+            this.btnProductCode.Size = new System.Drawing.Size(72, 13);
+            this.btnProductCode.TabIndex = 3;
+            this.btnProductCode.Text = "Product Code";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(132, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(600, 188);
+            this.Controls.Add(this.btnProductCode);
+            this.Controls.Add(this.btnManufacturerCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btncountrycode);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Barkod create";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox btncountrycode;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label btnManufacturerCode;
+        private System.Windows.Forms.Label btnProductCode;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ barkodolustur/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Printing; // yazmak için
+
+namespace barkodolustur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            PrintDocument doc = new PrintDocument(); //yazmak içim
+
+            Ean13Barcode2005.Ean13 barcode = new Ean13Barcode2005.Ean13();
+
+
+
+
+
+            //bu kod barkodun ilk 2 hanesi -ülke kodu
+
+            barcode.CountryCode = btncountrycode.Text;
+
+
+
+            //Bu kod üretici-imalatçı numarası -bu kısımın legal illegal gibi durumları da var
+
+            barcode.ManufacturerCode = textBox2.Text;
+
+
+
+            //Bu kod ürün kodu
+            barcode.ProductCode = textBox3.Text;
+
+            //Bu kısım boş geçilsede birşey değişmiyor EAN-13 te zaten 12 veri okuyorsunuz ,bu sayı  barkodun sonunda oluyor. kontrol kodu
+            barcode.ChecksumDigit = "5";
+
+
+            pictureBox1.Image = barcode.CreateBitmap();
+
+
+            Bitmap bitmap = new Bitmap(pictureBox1.Image);
+
+            Graphics grafik = Graphics.FromImage(bitmap);
+
+
+
+
+            Graphics g = pictureBox1.CreateGraphics();
+        }
+    }
+}
